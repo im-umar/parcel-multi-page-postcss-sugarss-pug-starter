836a6b6a28f3e7b89f3184f5b75d79a5c0d60ff5
Umar <umr.farq@gmail.com>
2019-01-11 17:57:47 +0000

    Initial Commit
    Initial Commit

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  map: false,
+  plugins: {
+    "postcss-import": {},
+    "postcss-simple-vars": {},
+    "postcss-nested": {},
+    "postcss-plugin": {},
+    "postcss-utilities": {},
+    "postcss-mixins": {},
+    "autoprefixer": {},
+    "postcss-circle": {},
+    "rucksack-css": {},
+    "cssnano": {},
+  }
+}
